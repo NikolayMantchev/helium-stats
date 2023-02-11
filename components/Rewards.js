c1a89a8e60e50a2ed199c2892b3938fb88c505f0
Mantchev <nik.mantchev@gmail.com>
2023-02-11 01:20:32 +0100

    time range for rewards

--- components/Rewards.js
+++ components/Rewards.js
@@ -1,25 +1,46 @@
 import useSWR from 'swr';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useRef } from 'react';
+const oneDay = 'sum?min_time=-1%20day'
+const sevenDays = 'sum?min_time=-7%20day'
+const thirtyDays = 'sum?min_time=-30%20day'
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function Rewards({ spot }) {
   const [rewardsData, setRewardsData] = useState([]);
-
-  const { data, error, isLoading } = useSWR(`https://api.helium.io/v1/hotspots/${spot}/rewards/sum?min_time=-1%20day`, fetcher, { refreshInterval: 600000 });
+  const [timeRange, setTimeRange] = useState(oneDay)
+  const checkRange = useRef()
+  const { data, error, isLoading } = useSWR(`https://api.helium.io/v1/hotspots/${spot}/rewards/${timeRange}`, fetcher, { refreshInterval: 600000 });
 
   useMemo(() => {
     const hotspot = { total: data?.data?.total, id: spot }
     setRewardsData(hotspot)
-  }, [data, spot])
+  }, [data, spot, timeRange])
 
+  // function submitRange(e) {
+  //   e.preventDefault()
+  //   setTimeRange(checkRange.current.value)
+  // }
+  const handleChange = (event) => {
+
+    setTimeRange(event.target.value);
+
+  };
   if (error) return <p className="title">Loading...</p>;
   if (isLoading) return <p className="title">{isLoading ? "Loading Rewards..." : null}</p>
   return (
     <>
       <div className="" key={rewardsData.id}>
         <h2 className="title">{rewardsData?.total?.toFixed(4)}</h2>
+        <select value={timeRange} onChange={handleChange}>
+          <option value={oneDay} >1D</option>
+          <option value={sevenDays}>7D</option>
+          <option value={thirtyDays}>30T</option>
+        </select>
       </div>
+      {/* <div className="btn btn__secondary" onClick={submitRange}>
+        1D
+      </div> */}
     </>
 
 
--- pages/api/endpoints.js
+++ pages/api/endpoints.js
@@ -6,31 +6,6 @@ const account = `accounts`
 const wallet = `${apiUrl}/${account}/${walletAddress}/${hotspotsString}`
 const rewards = `rewards`
 const daily = `sum?min_time=-1%20day`
-// const fetcher = (...args) => fetch(...args).then((res) => res.json())
-const hotspotAddressesString = Array.from([{ address: "112BVMQ98JL3WDEzReiBfQ25cq1EpozEoZ1CWFPUEPJ2dNPQMV73" },
-{ address: "1126DScbFyoQRf78RArnLnTR297ANmrqo9vWgCSSmdfCrC5kh7VS" },
-{ address: "11gvbTzyaQ6hSQyVfzS3w9iYoFx8hyZaXhiqeEibYzy7MAEHheD" },
-{ address: "11BfW8ptRiNUWzK17wQC7pewo2MWPQXrhSddC4DFEJ3PDUqBdtG" },
-{ address: "1124qvmAYxideExWT7jZUDaX4suKW7cUiSdssVP6f7RwMmTSrbTq" },
-{ address: "112L5HmcWrBcUFp5LL9oTaYt91hCCyQDuFGwqyeFPBBdtNrXRGwC" },
-{ address: "11QKn4Ezm9JaDiWJ8YcvnJgQo8NU8EKEi9rQFhKQ15KUx1cZAbu" },
-{ address: "11t41iv9SfXxVx4FZjrEe7JAibL6SQXforSqXamZtNSGM1FKvXf" },
-{ address: "112tq48yhU2ouYdeUHQ8dsQXFLyw8bBAm1FQK5UUKrKDPQqtCY44" },
-{ address: "112Lsvw4RvZ17EPoUMbMkidmPRLff5Vmuzdf1XeNHvanTsQgTXDN" },
-{ address: "112LuM4mL4ASHJcawdMzdyaXAV9wyHkNeXJRPaXS5kZRW4aGGCRF" },
-{ address: "1129M7yVcA4PGCCSecpCTJcYA6bHiqFggoUSYGmGbtXMzLTGjZEa" },
-{ address: "11wtpMo1oqPXZM8hvPm65R44HKdrbp35SF4qLfMpqzM7RncXKT2" },
-{ address: "112kdGA4MLa7USEadSC5WYm8ZMMLoVqmgYzg8ykffAUQJmEfLJS5" },
-{ address: "11x6RZnYsx1Ss4CtdiZaL37DBKB7BAc4G9Uy6onVfCjhJiY7PT3" },
-{ address: "11hHsSKfQJEUd45JzYa6Xx8vELpho5jCC8ub12mbQCKNzDBCiq6" },
-{ address: "112CiRNXKxZ7nDefTSV57LTs5aLxujy3tBGNAFi6Did9GyxoXv8G" },
-{ address: "112E4z3SzE4v6Je4dnL9c8jcMNMYSaRHVzQPDiu3j4FFtW2q4YAq" },
-{ address: "11UBDYqUYb5ttsSpcCwGmGr89GxSkouHdqM3yVzUj845NrQwu2U" },
-{ address: "11zkja97tvdPfGtZq9VERuJ2HrSmDftXvGWBPHY3nhpMJv1huNy" },
-{ address: "11jB6q4ziVrQB3YGeipChP6FKRWYqV372xyrxbepGkXqCW11toh" }])
-
-const hotspotAddresses = Array.from(hotspotAddressesString)
-
 
 const getHotspots = (walletAddress) => {
 	const transformedData = [];
@@ -76,4 +51,4 @@ const getCurentPrice = () => {
 }
 
 
-export { getHotspots, hotspotAddressesString, hotspotAddresses, getCurentPrice, getDaily };
+export { getHotspots, getCurentPrice, getDaily };
