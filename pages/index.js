8219e5d4ea138c12f292163eba2b430525a113dc
Mantchev <nik.mantchev@gmail.com>
2023-02-21 08:24:02 +0100

    TradingViewWidget added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 import CurPrice from "../components/CurPrice";
 import Dashboard from "../components/hotspots/Dashboard";
-
+import TradingViewWidget from "../components/TradingViewWidget";
 function Layout({ children }) {
 	return (
 		<>
@@ -12,8 +12,11 @@ function Layout({ children }) {
 					<div>
 						{children}
 						<div className="component__content">
-							<CurPrice></CurPrice>
+							<TradingViewWidget></TradingViewWidget>
 						</div>
+						{/* <div className="component__content">
+							<CurPrice></CurPrice>
+						</div> */}
 						<div className="component__content second_content">
 							<Dashboard></Dashboard>
 						</div>
